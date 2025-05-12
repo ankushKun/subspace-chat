e6832d9ef2da91c36214fb147c7b660b25b9362c
ankushKun <ankush4singh@gmail.com>
2025-05-13 00:42:22 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#181818] flex items-center justify-center font-mono">
-      <div className="bg-[#1f1f1f] rounded-lg shadow-lg w-full max-w-5xl mx-4 p-12 flex flex-col items-center justify-center" style={{ minHeight: '70vh' }}>
+    <div className="min-h-screen flex items-center justify-center font-mono" style={{
+      backgroundImage: "url(/stars.gif)",
+      backgroundSize: "cover",
+      backgroundPosition: "center center",
+      backgroundRepeat: "no-repeat",
+    }}>
+      <div className="rounded-lg shadow-lg w-full max-w-5xl mx-4 p-12 flex flex-col items-center justify-center" style={{ minHeight: '70vh' }}>
         <h1 className="text-6xl md:text-8xl font-bold mb-6 flex items-center space-x-2 select-none">
           <span className="text-[#444] font-normal" style={{ WebkitTextStroke: '1px #fff', color: 'transparent' }}>SUB</span>
           <span className="text-white font-bold">SPACE</span>

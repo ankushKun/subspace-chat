7fee2a6771335f6e349bd36c96ce4dfb3c5632d6
ankushKun <ankush4singh@gmail.com>
2025-05-14 16:58:08 +0530

    checkpoint

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,10 +1,11 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import sLogo from "@/assets/s.png"
 
 export default function Hero() {
     return (
         <div className='w-full h-full flex flex-col items-center justify-center select-none'>
             <ModeToggle className="absolute top-4 right-4" />
-            <img src="/s.png" className='w-24 h-24 drop-shadow-lg drop-shadow-foreground/20' draggable={false} />
+            <img src={sLogo} className='w-24 h-24 drop-shadow-lg drop-shadow-foreground/20' draggable={false} />
             <div className="text-4xl font-bold text-foreground/70">Subspace Chat</div>
             <p className="text-sm mt-2 text-muted-foreground">Your intergalactic communications system</p>
             <div className="h-20" />

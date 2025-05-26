56581e13ab357f43cd986ae9356473f60c3c7765
ankushKun <ankush4singh@gmail.com>
2025-05-26 11:01:33 +0530

    harder better faster stronger

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -7,10 +7,13 @@ export default function App() {
 
   return (
     <div className="flex flex-row items-start justify-start h-svh p-2">
-      <ServerList className="w-[80px] min-w-[80px] max-w-[80px] h-svh" />
+      {/* <ServerList className="w-[80px] min-w-[80px] max-w-[80px] h-svh" />
       <ChannelList className="w-[350px] min-w-[350px] max-w-[350px]" />
       <MessageList className="grow h-svh" />
-      <MemberList className="w-[269px] min-w-[269px] max-w-[269px] h-svh" />
+      <MemberList className="w-[269px] min-w-[269px] max-w-[269px] h-svh" /> */}
+      <div>
+        Hi, this is being updated :)
+      </div>
     </div>
   )
 }
